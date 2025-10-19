012b831f4484dea348a7438b418c0a07476611fe
user <vanyshev.matvei@yandex.ru>
2025-10-19 22:14:44 +0500

    fix_bugs

--- src/js/ui/components/initExceeding.js
+++
@@ -1,8 +0,0 @@
-import { exceedingData } from "../../mockData/exceedingData.js";
-import { exceedingTemplate } from "../templates/exceedingTemplate.js";
-
-const initExceeding = (exceeding_all_expectationsNode) => {
-  exceeding_all_expectationsNode.insertAdjacentHTML("beforeend", exceedingTemplate(exceedingData));
-};
-
-export default initExceeding;
